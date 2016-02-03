64edaeb992c1e70df7df2dd31cf3f8b77614cb38
Chris Harrington <ironiridis@gmail.com>
2016-02-03 01:17:09 -0600

    nope, gcc abi fix isnt idempotent

--- maint.sh
+++ maint.sh
@@ -20,7 +20,6 @@ try() {
 
 try "Verify up-to-date Portage" "emerge -1u portage"
 try "Check for gentoolkit" "emerge -1u gentoolkit"
-try "Rebuild packages broken by GCC 5 ABI" "revdep-rebuild --library 'libstdc++.so.6' -- --exclude gcc"
 if [[ $(find /usr/lib*/python3* -name '*cpython-3[3-5].so | wc -l) -gt 0 ]]
 then try "Fix Python 3 ABI bustage" "emerge -1 $(find /usr/lib*/python3* -name '*cpython-3[3-5].so')"
 fi
